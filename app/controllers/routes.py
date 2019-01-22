7a37d04a765315ac99bb307903764b4f0589634b
Alcides Mignoso <alcides.mignoso@catijr.com.br>
2019-01-22 13:03:25 -0200

    .

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -1,8 +1,8 @@
-from flask import flash, render_template, request, redirect, url_for, session
-from models.models import Usuario
+from flask import render_template, request, redirect, url_for, session
+from app.models.models import Usuario
 from controllers.forms import LoginForm
 from controllers.functions import check_password
-from flask_login import login_manager, login_required, login_user, logout_user, current_user
+from flask_login import login_required, login_user, logout_user, current_user
 from flask_app import app
 
 @app.route('/')
