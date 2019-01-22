15db3eace315980d9e8051a8e2625b3a133d855f
matheusvictorello <matheusvictorello@gmail.com>
2019-01-22 19:19:20 -0200

    Melhoria verificação de email

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,14 @@ from flask import Flask
 from flask_script import Manager
 
 app = Flask(__name__)
+app.config.update(
+	#EMAIL SETTINGS
+	MAIL_SERVER='smtp.gmail.com',
+	MAIL_PORT=465,
+	MAIL_USE_SSL=True,
+	#MAIL_USERNAME = env
+	#MAIL_PASSWORD = env
+	)
 
 from app.controllers import routes
 
