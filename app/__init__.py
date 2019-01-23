a1b89666d7790e0685af1710e9ce7a8b494db50a
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-01-23 00:16:40 -0200

    Melhorando importação de configuração

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,18 @@ from flask import Flask
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+import os
+
+config = {
+    "development": 'config.default',
+    'production': 'config.production',
+    'default': 'config.default'
+}
+
+config_name = os.getenv('FLASK_CONFIGURATION', 'default')
 
 app = Flask(__name__)
-app.config.from_object('config.default')
+app.config.from_object(config[config_name])
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
