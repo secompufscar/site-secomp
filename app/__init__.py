497d608fec5b20e420345ba96f561a722cbb1b27
nathanoliveira19 <nathan.oliveirasc17@gmail.com>
2019-01-23 17:44:00 -0200

    Adicionado smtp do zoho e algumas correções

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,17 @@ from flask_script import Manager
 from dotenv import load_dotenv
 import os
 
-load_dotenv() 
+load_dotenv()
 app = Flask(__name__)
 app.config.update(
 	#EMAIL SETTINGS
-	MAIL_SERVER='smtp.gmail.com',
+	MAIL_SERVER='smtp.zoho.com',
 	MAIL_PORT=465,
 	MAIL_USE_SSL=True,
-	MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+	MAIL_USERNAME = os.getenv('MAIL_USERNAME'),
 	MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 	)
 
 from app.controllers import routes
 
 manager = Manager(app)
-
--- app/controllers/functions.py
+++ app/controllers/functions.py
@@ -1,18 +1,20 @@
+from flask import url_for
+
 def check_password(x, y):
     #Esperar criptografia para programar a função que verificará a integridade da senha
     return True
 
 def enviarEmailConfirmacao(app, email, token): #Envia email para validação do email
 	from flask_mail import Mail, Message
-	
+
 	mail = Mail(app)
-	
+
 	#Cria a msg, Assunto, De, Para
 	msg = Message('Confirmação do Email', sender=app.config['MAIL_USERNAME'], recipients=[email])
 	#Str com o link da verificação + token
 	link = url_for('verificacao', token=token, _external=True)
 	msg.body = '{}'.format(link)
-	
+
 	try:
 		mail.send(msg) #Envia o email
 	except Exception as e: #Erros mais prováveis são devivo ao email_config, printa error em um arquivo
