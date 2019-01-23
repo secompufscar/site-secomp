6daf2c58b9f9145043948c57df48911b468e4294
Lorhan Sohaky <lorhansohaky@gmail.com>
2019-01-23 01:12:03 -0200

    Utilizando LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from flask_login import LoginManager
 import os
 
 config = {
@@ -14,8 +15,12 @@ config_name = os.getenv('FLASK_CONFIGURATION', 'default')
 
 app = Flask(__name__)
 app.config.from_object(config[config_name])
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 from app.controllers import routes
 
