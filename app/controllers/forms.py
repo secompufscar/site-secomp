f0844f46439618e3430ab25241fd96447de5131e
kah_mayumi <kahjohansson@gmail.com>
2019-02-06 18:16:03 -0200

    Correção dos models e criação dos forms da área administrativa

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -16,7 +16,53 @@ class CadastroForm(FlaskForm):
     curso = SelectField('Curso', choices=escolhas_curso, id= "curso")
     instituicao = SelectField('Instituição', choices=escolhas_instituicao, id= "instituicao", default="UFSCar")
     cidade = SelectField('Cidade', choices=escolhas_cidade, id= "cidade", default="São Carlos")
-    data_nasc = DateField("Data de Nascimento", format="%d/%m/%Y", id="data_nasc")
-	
+    data_nasc = DateField('Data de Nascimento', format='%d/%m/%Y', id="data_nasc")
+
+class CadastroInformacoesPessoais(FlaskForm):
+    nome_completo = StringField('Nome Completo', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=80)])
+    email = StringField('Email', validators=[InputRequired(), Email(message=ERRO_EMAIL), Length(min=1, max=254)])
+    telefone = StringField('Telefone', validators=[InputRequired(), Length(min=1, max=11)])
+    profissao = StringField('Profissão', validators=[InputRequired(), Length(min=1, max=64)])
+    empresa_universidade = StringField('Empresa/Universidade')
+    biografia = StringField('Biografia', validators=[InputRequired(), Length(min=1, max=1500)])
+    foto = StringField('Foto')
+    tamanho_camiseta = SelectField('Tamanho de Camiseta', choices=escolhas_camiseta, id="tamanho_camiseta")
+    facebook = StringField('Facebook')
+    twitter = StringField('Twitter')
+    linkedin = StringField('Linkedin')
+    github = StringField('GitHub')
+
+class CadastroInformacoesMinicurso(FlaskForm):
+    titulo = StringField('Título', validators=[InputRequired(), Length(min=1,max=64)])
+    area = SelectField('Área', validators=[InputRequired()])
+    descricao = StringField('Descrição', validators=[InputRequired(), Length(min=1,max=1024)])
+    pre_requisitos = StringField('Pré-requisitos do participante')
+    planejamento = StringField('Planejamento de como será')
+    apresentacao_extra = StringField('Apresentação Extra')
+    material = StringField('Descrição')
+    requisitos_hardware = StringField('Requisitos de Hardware')
+    requisitos_software = StringField('Requisitos de Software')
+    dicas_instalacao = StringField('Dicas para instalação dos softwares necessários')
+    observacoes = StringField('Observações')
+
+class CadastroInformacoesPalestra(FlaskForm):
+    titulo = StringField('Título', validators=[InputRequired(), Length(min=1,max=64)])
+    area = SelectField('Área', validators=[InputRequired()])
+    descricao = StringField('Descrição', validators=[InputRequired(), Length(min=1,max=1024)])
+    requisitos_hardware = StringField('Requisitos de Hardware')
+    planejamento = StringField('Planejamento de como será')
+    apresentacao_extra = StringField('Apresentação Extra')
+    material = StringField('Descrição')
+    observacoes = StringField('Observações')
+
+class CadastroInformaçõesLocomoçõesEstadia(FlaskForm):
+    cidade_origem = StringField('Título', validators=[InputRequired(), Length(min=1,max=64)])
+    data_chegada_sanca = DateField('Data de Chegada em São Carlos', format='%d/%m/%Y', id="data_chegada_sanca")
+    data_partida_sanca = DateField('Data de Partida de São Carlos', format='%d/%m/%Y', id="data_partida_sanca")
+    transporte_ida_volta = BooleanField('Requer que a SECOMP UFSCar pague por seu transporte de ida e volta')
+    hotel = BooleanField('Requer que a SECOMP UFSCar pague por seu hotel?')
+    transporte_sanca = BooleanField('Requer que a SECOMP UFSCar se encarregue do seu transporte dentro de São Carlos?')
+    observacoes = StringField('Observações')
+
 class CadastroAtividadeForm(FlaskForm):
-   titulo = StringField('Título da Atividade', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=30)])
+   titulo = StringField('Título da Atividade', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=64)])
--- app/models/models.py
+++ app/models/models.py
@@ -1,18 +1,18 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Date
 from app import db
+from app.models.relationships import *
 
 
 class Usuario(db.Model):
     __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    participantes_associados = db.relationship('participante', backref='usuario', lazy=True)
     email = Column(String(64), unique=True, nullable=False)
     senha = Column(String(256), nullable=False)
     ultimo_login = Column(DateTime, nullable=False)
     data_cadastro = Column(DateTime, nullable=False)
     permissao = Column(Integer, nullable=False)
     primeiro_nome = Column(String(64), nullable=False)
-    ult_nome = Column(String(64), nullable=False)
+    sobrenome = Column(String(64), nullable=False)
     curso = Column(String(64), nullable=False)
     cidade = Column(String(64), nullable=False)
     instituicao = Column(String(64), nullable=False)
@@ -21,6 +21,7 @@ class Usuario(db.Model):
     autenticado = Column(Boolean, default=False)
     email_verificado = Column(Boolean, default=False)
     salt = Column(String(30), nullable=False)
+    participantes_associados = db.relationship('Participante', backref='usuario', lazy=True)
 
     def is_active(self):
         return True
@@ -37,18 +38,22 @@ class Usuario(db.Model):
 
 class Participante(db.Model):
     __tablename__ = 'participante'
-    id = Column(Integer, db.ForeignKey('usuario.id'), primary_key=True)
+    id = Column(Integer, primary_key=True)
+    id_usuario = Column(Integer, db.ForeignKey('usuario.id'))
     edicao = Column(Integer, nullable=False)
     pacote = Column(Boolean, nullable=False)
     pagamento = Column(Boolean, nullable=False)
     camiseta = Column(String(3))
     data_inscricao = Column(DateTime, nullable=False)
     credenciado = Column(Boolean, nullable=False)
+    atividade = db.relationship('Atividade', secondary=relacao_atividade_participante, lazy=True,
+        back_populates='participante')
+
 
 
 class Ministrante(db.Model):
     __tablename__ = 'ministrante'
-    id = Column(Integer, db.ForeignKey('ministrante.id'), primary_key=True)
+    id = Column(Integer, primary_key=True)
     nome_completo = Column(String(80), nullable=False)
     email = Column(String(64), nullable=False)
     telefone = Column(String(64), nullable=False)
@@ -68,8 +73,6 @@ class Ministrante(db.Model):
     hotel = Column(Boolean, nullable=False)
     transporte_sanca = Column(Boolean, nullable=False)
     observacoes = Column(String(512))
-
-
 
 class Atividade(db.Model):
     __tablename__ = 'atividade'
@@ -91,14 +94,10 @@ class Atividade(db.Model):
     tipo = Column(Integer, nullable=False)
     data_hora = Column(DateTime, nullable=False)
     local = Column(String(64), nullable=False)
-    ministrante = db.relationship('ministrante', backref='ministrante', lazy=True)
-    inscritos = db.relationship('participante', secondary=relacao_atividade_participante, lazy=True,
-        backref=db.backref('atividade', lazy='subquery'))
-
-
-relacao_atividade_participante = db.Table('relacao_atividade_participante',
-                                       Column('id_atividade', Integer, db.ForeignKey('atividade.id'), primary_key=True),
-                                       Column('id_participante', Integer, db.ForeignKey('participante.id'), primary_key=True))
+    ministrante = db.relationship('Ministrante', secondary=relacao_atividade_ministrante, lazy=True,
+        back_populates='atividade')
+    participante = db.relationship('Participante', secondary=relacao_atividade_participante, lazy=True,
+        back_populates='atividade')
 
 #if __name__ == "__main__":
 #    db.create_all()
