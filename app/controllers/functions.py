411778dfc05636186bba3e9d2b6b4ac00e8c242e
nathanoliveira19 <nathan.oliveirasc17@gmail.com>
2019-02-10 20:18:05 -0200

    Criados model de Evento e função de dicionário para participantes

--- app/controllers/functions.py
+++ app/controllers/functions.py
@@ -40,3 +40,19 @@ def get_dicionario_usuario(usuario):
 		"data_nasc": usuario.data_nasc
 	}
 	return info
+	
+def get_dicionario_info_evento(edicao):
+	#TODO: Quando pronto o model de evento, completar com todas as informações
+	evento = db.session.query(Evento).filter_by(edicao=edicao).first()
+	participante = db.session.query(Participante).filter_by(id_evento=evento.id, id_usuario=current_user.id).first()
+	info = {
+		"titulo": str(evento.edicao) + "ª SECOMP UFSCar",
+		"data_inscricao" : participante.data_inscricao,
+		"presencas": ["Credenciamento", "Abertura"],
+		"kit_pago": participante.pagamento,
+		"camiseta": participante.camiseta,
+		"opcao_coffee": participante.opcao_coffee,
+		"score_geral": 10000
+
+	}
+	return info
