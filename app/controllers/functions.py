a5e8561159a15cec201e104090fcc2ca2465bfac
nathanoliveira19 <nathan.oliveirasc17@gmail.com>
2019-02-25 12:30:33 -0300

    Adicionado try/except em functions.py

--- app/controllers/functions.py
+++ app/controllers/functions.py
@@ -31,38 +31,50 @@ def email_confirmado():
 	return usuario.email_verificado
 
 def get_opcoes_cidades():
-	cidades = db.session.query(Cidade).filter_by().all()
-	info_cidades = []
-	for cidade in cidades:
-		info = (cidade.id, cidade.nome)
-		info_cidades.append(info)
-	return info_cidades
+	try:
+		cidades = db.session.query(Cidade).filter_by().all()
+		info_cidades = []
+		for cidade in cidades:
+			info = (cidade.id, cidade.nome)
+			info_cidades.append(info)
+		return info_cidades
+	except:
+		return None
 
 def get_opcoes_instituicoes():
-	instituicoes = db.session.query(Instituicao).filter_by().all()
-	info_instituicoes = []
-	for instituicao in instituicoes:
-		info = (instituicao.id, instituicao.nome)
-		info_instituicoes.append(info)
-	return info_instituicoes
+	try:
+		instituicoes = db.session.query(Instituicao).filter_by().all()
+		info_instituicoes = []
+		for instituicao in instituicoes:
+			info = (instituicao.id, instituicao.nome)
+			info_instituicoes.append(info)
+		return info_instituicoes
+	except:
+		return None
 
 def get_opcoes_cursos():
-	cursos = db.session.query(Curso).filter_by().all()
-	info_cursos = []
-	for curso in cursos:
-		info = (curso.id, curso.nome)
-		info_cursos.append(info)
-	return info_cursos
-
+	try:
+		cursos = db.session.query(Curso).filter_by().all()
+		info_cursos = []
+		for curso in cursos:
+			info = (curso.id, curso.nome)
+			info_cursos.append(info)
+		return info_cursos
+	except:
+		return None
+		
 def get_opcoes_camisetas():
-	camisetas = db.session.query(Camiseta).filter_by().order_by(Camiseta.ordem_site).all()
-	info_camisetas = []
-	for camiseta in camisetas:
-		if camiseta.quantidade_restante > 0:
-			info = (camiseta.id, camiseta.tamanho)
-			info_camisetas.append(info)
+	try:
+		camisetas = db.session.query(Camiseta).filter_by().order_by(Camiseta.ordem_site).all()
+		info_camisetas = []
+		for camiseta in camisetas:
+			if camiseta.quantidade_restante > 0:
+				info = (camiseta.id, camiseta.tamanho)
+				info_camisetas.append(info)
 
-	return info_camisetas
+		return info_camisetas
+	except:
+		return None
 
 def get_dicionario_usuario(usuario):
 	info = {
