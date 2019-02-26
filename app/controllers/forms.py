13411a4d1afc71388a200825d6d9a1b5ed320c8a
Gabriel <g4briel.4lves@gmail.com>
2019-02-25 22:47:16 -0300

    Melhorando estilo de código

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -6,30 +6,45 @@ from app.controllers.constants import *
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired(), Email(message=ERRO_EMAIL), Length(min=1, max=254)])
-    senha = PasswordField('Senha', validators=[InputRequired(), Length(min=8, max=20)])
+    email = StringField('Email', validators=[InputRequired(), Email(
+        message=ERRO_EMAIL), Length(min=1, max=254)])
+    senha = PasswordField('Senha', validators=[
+                          InputRequired(), Length(min=8, max=20)])
 
 
 class CadastroForm(FlaskForm):
-    primeiro_nome = StringField('Primeiro Nome', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=30)])
-    sobrenome = StringField('Sobrenome', validators=[InputRequired(), Length(min=1, max=100)])
-    email = StringField('Email', validators=[InputRequired(), Email(message=ERRO_EMAIL), Length(min=1, max=254)])
-    senha = PasswordField('Senha', validators=[InputRequired(), EqualTo('confirmacao', message=ERRO_COMPARA_SENHAS), Length(min=8, max=20)])
-    confirmacao = PasswordField('Confirmação de Senha', validators=[InputRequired(), Length(min=8, max=20)])
-    curso = SelectField('Curso', choices=get_opcoes_cursos(), id= "curso", coerce=int)
-    instituicao = SelectField('Instituição', choices=get_opcoes_instituicoes(), id="instituicao", default="UFSCar", coerce=int)
-    cidade = SelectField('Cidade', choices=get_opcoes_cidades(), id="cidade", default="São Carlos", coerce=int)
-    data_nasc = DateField("Data de Nascimento", format="%d/%m/%Y", id="data_nasc")
+    primeiro_nome = StringField('Primeiro Nome', validators=[InputRequired(
+        message=ERRO_INPUT_REQUIRED), Length(min=1, max=30)])
+    sobrenome = StringField('Sobrenome', validators=[
+                            InputRequired(), Length(min=1, max=100)])
+    email = StringField('Email', validators=[InputRequired(), Email(
+        message=ERRO_EMAIL), Length(min=1, max=254)])
+    senha = PasswordField('Senha', validators=[InputRequired(), EqualTo(
+        'confirmacao', message=ERRO_COMPARA_SENHAS), Length(min=8, max=20)])
+    confirmacao = PasswordField('Confirmação de Senha', validators=[
+                                InputRequired(), Length(min=8, max=20)])
+    curso = SelectField('Curso', choices=get_opcoes_cursos(),
+                        id="curso", coerce=int)
+    instituicao = SelectField('Instituição', choices=get_opcoes_instituicoes(
+    ), id="instituicao", default="UFSCar", coerce=int)
+    cidade = SelectField('Cidade', choices=get_opcoes_cidades(
+    ), id="cidade", default="São Carlos", coerce=int)
+    data_nasc = DateField("Data de Nascimento",
+                          format="%d/%m/%Y", id="data_nasc")
 
 
 class ContatoForm(FlaskForm):
-	nome_completo = StringField('Nome', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=30)])
-	email = StringField('Email', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=30)])
-	mensagem = StringField('Mensagem', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=500)])
+    nome_completo = StringField('Nome', validators=[InputRequired(
+        message=ERRO_INPUT_REQUIRED), Length(min=1, max=30)])
+    email = StringField('Email', validators=[InputRequired(
+        message=ERRO_INPUT_REQUIRED), Length(min=1, max=30)])
+    mensagem = StringField('Mensagem', validators=[InputRequired(
+        message=ERRO_INPUT_REQUIRED), Length(min=1, max=500)])
 
 
 class ParticipanteForm(FlaskForm):
     kit = BooleanField('Kit', validators=[InputRequired()], id="kit")
-    camiseta = SelectField('Camiseta', choices=get_opcoes_camisetas(), id= "camiseta", default="P Feminino", coerce=int)
-    restricao_coffee = SelectField('Restrição para o Coffee-Break', choices=escolhas_restricao, default="Nenhum", coerce=int )
-
+    camiseta = SelectField('Camiseta', choices=get_opcoes_camisetas(
+    ), id="camiseta", default="P Feminino", coerce=int)
+    restricao_coffee = SelectField(
+        'Restrição para o Coffee-Break', choices=escolhas_restricao, default="Nenhum", coerce=int)
--- app/controllers/functions.py
+++ app/controllers/functions.py
@@ -7,150 +7,170 @@ from app.controllers.constants import *
 from app.models.models import *
 
 
-def enviarEmailConfirmacao(app, email, token): #Envia email para validação do email
-	from flask_mail import Mail, Message
+# Envia email para validação do email
+def enviarEmailConfirmacao(app, email, token):
+    from flask_mail import Mail, Message
 
-	mail = Mail(app)
+    mail = Mail(app)
 
-	#Cria a msg, Assunto, De, Para
-	msg = Message('Confirmação do Email', sender=app.config['MAIL_USERNAME'], recipients=[email])
-	#Str com o link da verificação + tokmethods=['POST', 'GET']
-	link = url_for('verificacao', token=token, _external=True)
-	msg.body = '{}'.format(link)
+    # Cria a msg, Assunto, De, Para
+    msg = Message('Confirmação do Email',
+                  sender=app.config['MAIL_USERNAME'], recipients=[email])
+    # Str com o link da verificação + tokmethods=['POST', 'GET']
+    link = url_for('verificacao', token=token, _external=True)
+    msg.body = '{}'.format(link)
 
-	try:
-		mail.send(msg) #Envia o email
-	except Exception as e: #Erros mais prováveis são devivo ao email_config, printa error em um arquivo
-		try:
-			log = open('logMailError.txt', 'a+')
-			log.write('{} {} {}\n'.format(str(e), email, strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
-			log.close()
-		except:
-			pass
-			
+    try:
+        mail.send(msg)  # Envia o email
+    except Exception as e:  # Erros mais prováveis são devivo ao email_config, printa error em um arquivo
+        try:
+            log = open('logMailError.txt', 'a+')
+            log.write('{} {} {}\n'.format(str(e), email,
+                                          strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+            log.close()
+        except:
+            pass
+
+
 def enviarEmailDM(app, nome, email, mensagem):
-	from flask_mail import Mail, Message
-	
-	mail = Mail(app)
-	
-	msg = Message('Mensagem de {}'.format(nome), sender=app.config['MAIL_USERNAME'], recipients=app.config['MAIL_DM'])
-	msg.body = '{}\nEmail: {}\n\n{}'.format(nome, email, mensagem)
+    from flask_mail import Mail, Message
 
-	try:
-		mail.send(msg) #Envia o email
-	except Exception as e: #Erros mais prováveis são devivo ao email_config, printa error em um arquivo
-		try:
-			log = open('logMailError.txt', 'a+')
-			log.write('{} {} {} {} {}\n'.format(str(e), nome, email, mensagem, strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
-			log.close()
-		except:
-			pass
+    mail = Mail(app)
+
+    msg = Message('Mensagem de {}'.format(
+        nome), sender=app.config['MAIL_USERNAME'], recipients=app.config['MAIL_DM'])
+    msg.body = '{}\nEmail: {}\n\n{}'.format(nome, email, mensagem)
+
+    try:
+        mail.send(msg)  # Envia o email
+    except Exception as e:  # Erros mais prováveis são devivo ao email_config, printa error em um arquivo
+        try:
+            log = open('logMailError.txt', 'a+')
+            log.write('{} {} {} {} {}\n'.format(str(e), nome, email,
+                                                mensagem, strftime("%a, %d %b %Y %H:%M:%S", gmtime())))
+            log.close()
+        except:
+            pass
+
 
 def email_confirmado():
-	usuario = current_user
-	usuario = db.session.query(Usuario).filter_by(email=usuario.email).first()
-	return usuario.email_verificado
+    usuario = current_user
+    usuario = db.session.query(Usuario).filter_by(email=usuario.email).first()
+    return usuario.email_verificado
+
 
 def get_opcoes_cidades():
-	cidades = db.session.query(Cidade).filter_by().all()
-	info_cidades = []
-	for cidade in cidades:
-		info = (cidade.id, cidade.nome)
-		info_cidades.append(info)
-	return info_cidades
+    cidades = db.session.query(Cidade).filter_by().all()
+    info_cidades = []
+    for cidade in cidades:
+        info = (cidade.id, cidade.nome)
+        info_cidades.append(info)
+    return info_cidades
+
 
 def get_opcoes_instituicoes():
-	instituicoes = db.session.query(Instituicao).filter_by().all()
-	info_instituicoes = []
-	for instituicao in instituicoes:
-		info = (instituicao.id, instituicao.nome)
-		info_instituicoes.append(info)
-	return info_instituicoes
+    instituicoes = db.session.query(Instituicao).filter_by().all()
+    info_instituicoes = []
+    for instituicao in instituicoes:
+        info = (instituicao.id, instituicao.nome)
+        info_instituicoes.append(info)
+    return info_instituicoes
+
 
 def get_opcoes_cursos():
-	cursos = db.session.query(Curso).filter_by().all()
-	info_cursos = []
-	for curso in cursos:
-		info = (curso.id, curso.nome)
-		info_cursos.append(info)
-	return info_cursos
+    cursos = db.session.query(Curso).filter_by().all()
+    info_cursos = []
+    for curso in cursos:
+        info = (curso.id, curso.nome)
+        info_cursos.append(info)
+    return info_cursos
+
 
 def get_opcoes_camisetas():
-	camisetas = db.session.query(Camiseta).filter_by().order_by(Camiseta.ordem_site).all()
-	info_camisetas = []
-	for camiseta in camisetas:
-		if camiseta.quantidade_restante > 0:
-			info = (camiseta.id, camiseta.tamanho)
-			info_camisetas.append(info)
+    camisetas = db.session.query(Camiseta).filter_by().order_by(
+        Camiseta.ordem_site).all()
+    info_camisetas = []
+    for camiseta in camisetas:
+        if camiseta.quantidade_restante > 0:
+            info = (camiseta.id, camiseta.tamanho)
+            info_camisetas.append(info)
 
-	return info_camisetas
+    return info_camisetas
+
 
 def get_dicionario_usuario(usuario):
-	info = {
+    info = {
         "nome": usuario.primeiro_nome + ' ' + usuario.sobrenome,
-		"email": usuario.email,
-		"curso": usuario.curso,
-		"instituicao": usuario.instituicao,
-		"data_nasc": usuario.data_nascimento
-	}
-	return info
+        "email": usuario.email,
+        "curso": usuario.curso,
+        "instituicao": usuario.instituicao,
+        "data_nasc": usuario.data_nascimento
+    }
+    return info
+
 
 def get_score_evento(edicao):
-	return 10000
+    return 10000
 
-#Função usada na dashboard do usuário
+# Função usada na dashboard do usuário
+
+
 def get_dicionario_eventos_participante(base_url):
-	info_eventos = []
-	agora = datetime.datetime.now()
-	participantes = db.session.query(Participante).filter_by(id_usuario=current_user.id).all()
-	ja_participa = False
-	for participante in participantes:
-		evento = participante.evento
-		if evento.edicao != EDICAO_ATUAL:
-			info = {
-				"titulo": str(evento.edicao) + "ª SECOMP UFSCar",
-				"edicao": evento.edicao,
-				"participantes": len(evento.participantes),
-				"url": base_url + "/evento/" + str(evento.edicao),
-				"inscricao": 0
-			}
-			info_eventos.append(info)
-		else:
-			ja_participa = True
-	evento = db.session.query(Evento).filter_by(edicao=EDICAO_ATUAL).first()
-	if ja_participa == False:
-		if agora >= evento.inicio_inscricoes_evento and agora < evento.fim_inscricoes_evento:
-			inscricao = 1
-		else:
-			inscricao = 2
-	else:
-		inscricao = 0
-	info = {
-		"titulo": str(evento.edicao) + "ª SECOMP UFSCar",
-		"edicao": evento.edicao,
-		"participantes": len(evento.participantes),
-		"url": base_url + "/evento/" + str(evento.edicao),
-		"inscricao": inscricao
-	}
-	info_eventos.append(info)
-	return info_eventos
+    info_eventos = []
+    agora = datetime.datetime.now()
+    participantes = db.session.query(Participante).filter_by(
+        id_usuario=current_user.id).all()
+    ja_participa = False
+    for participante in participantes:
+        evento = participante.evento
+        if evento.edicao != EDICAO_ATUAL:
+            info = {
+                "titulo": str(evento.edicao) + "ª SECOMP UFSCar",
+                "edicao": evento.edicao,
+                "participantes": len(evento.participantes),
+                "url": base_url + "/evento/" + str(evento.edicao),
+                "inscricao": 0
+            }
+            info_eventos.append(info)
+        else:
+            ja_participa = True
+    evento = db.session.query(Evento).filter_by(edicao=EDICAO_ATUAL).first()
+    if ja_participa == False:
+        if agora >= evento.inicio_inscricoes_evento and agora < evento.fim_inscricoes_evento:
+            inscricao = 1
+        else:
+            inscricao = 2
+    else:
+        inscricao = 0
+    info = {
+        "titulo": str(evento.edicao) + "ª SECOMP UFSCar",
+        "edicao": evento.edicao,
+        "participantes": len(evento.participantes),
+        "url": base_url + "/evento/" + str(evento.edicao),
+        "inscricao": inscricao
+    }
+    info_eventos.append(info)
+    return info_eventos
 
-#Função usada na página de informações de um determinado evento
+# Função usada na página de informações de um determinado evento
+
+
 def get_dicionario_info_evento(edicao):
-	evento = db.session.query(Evento).filter_by(edicao=edicao).first()
-	participante = db.session.query(Participante).filter_by(id_evento=evento.id, id_usuario=current_user.id).first()
-	presencas = participante.presencas
-	atividades = []
-	for presenca in presencas:
-		atividades.append(presenca.atividades.titulo)
+    evento = db.session.query(Evento).filter_by(edicao=edicao).first()
+    participante = db.session.query(Participante).filter_by(
+        id_evento=evento.id, id_usuario=current_user.id).first()
+    presencas = participante.presencas
+    atividades = []
+    for presenca in presencas:
+        atividades.append(presenca.atividades.titulo)
 
-	info = {
-		"titulo": str(evento.edicao) + "ª SECOMP UFSCar",
-		"data_inscricao" : participante.data_inscricao,
-		"presencas": atividades,
-		"kit_pago": participante.pagamento,
-		"camiseta": participante.camiseta,
-		"opcao_coffee": participante.opcao_coffee,
-		"score_geral": get_score_evento(edicao)
-	}
-	return info
+    info = {
+        "titulo": str(evento.edicao) + "ª SECOMP UFSCar",
+        "data_inscricao": participante.data_inscricao,
+        "presencas": atividades,
+        "kit_pago": participante.pagamento,
+        "camiseta": participante.camiseta,
+        "opcao_coffee": participante.opcao_coffee,
+        "score_geral": get_score_evento(edicao)
+    }
+    return info
