a97787956014bdc722b4c1b04c59c4ca5de61046
Gabriel <g4briel.4lves@gmail.com>
2019-02-25 21:52:18 -0300

    Resolvendo issues apontadas pelo codacy

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SelectField, DateField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SelectField, DateField
 from wtforms.validators import InputRequired, Email, Length, EqualTo
 from app.controllers.functions import get_opcoes_cidades, get_opcoes_instituicoes, get_opcoes_cursos, get_opcoes_camisetas
 from app.controllers.constants import *
 
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email(message=ERRO_EMAIL), Length(min=1, max=254)])
     senha = PasswordField('Senha', validators=[InputRequired(), Length(min=8, max=20)])
+
 
 class CadastroForm(FlaskForm):
     primeiro_nome = StringField('Primeiro Nome', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=30)])
@@ -18,11 +20,13 @@ class CadastroForm(FlaskForm):
     instituicao = SelectField('Instituição', choices=get_opcoes_instituicoes(), id="instituicao", default="UFSCar", coerce=int)
     cidade = SelectField('Cidade', choices=get_opcoes_cidades(), id="cidade", default="São Carlos", coerce=int)
     data_nasc = DateField("Data de Nascimento", format="%d/%m/%Y", id="data_nasc")
-	
+
+
 class ContatoForm(FlaskForm):
 	nome_completo = StringField('Nome', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=30)])
 	email = StringField('Email', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=30)])
 	mensagem = StringField('Mensagem', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=500)])
+
 
 class ParticipanteForm(FlaskForm):
     kit = BooleanField('Kit', validators=[InputRequired()], id="kit")
