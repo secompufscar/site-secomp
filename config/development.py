1a95634a40cedc15ed97b5122544b6654131e49d
nathanoliveira19 <nathan.oliveirasc17@gmail.com>
2019-02-27 07:41:39 -0300

    Correção no development.py

--- config/development.py
+++ config/development.py
@@ -7,16 +7,16 @@ DEBUG = None
 SECRET_KEY = os.urandom(32)
 
 # FLASK MAIL
-MAIL_SERVER = 'smtp.zoho.com'
+MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
-MAIL_USERNAME = 'noreply@secompufscar.com.br'
+MAIL_USERNAME = 'email@gmail.com.br'
 MAIL_PASSWORD= 'passmail'
 
 # FLASK ADMIN
 FLASK_ADMIN_SWATCH = 'darkly'
 
 # SQLALCHEMY
-SQLALCHEMY_DATABASE_URI = 'mysql://root:2968@localhost/db'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
