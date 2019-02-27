b86592255a264fb7c35228951c68f0ad8b400246
nathanoliveira19 <nathan.oliveirasc17@gmail.com>
2019-02-27 15:02:01 -0300

    Atualizadas mensagens de erro nos forms

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -6,15 +6,15 @@ from app.controllers.constants import *
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired(), Email(message=ERRO_EMAIL), Length(min=1, max=254)])
-    senha = PasswordField('Senha', validators=[InputRequired(), Length(min=8, max=20)])
+    email = StringField('Email', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Email(message=ERRO_EMAIL), Length(min=1, max=254)])
+    senha = PasswordField('Senha', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=8, max=20, message=ERRO_TAMANHO_SENHA)])
 
 class CadastroForm(FlaskForm):
     primeiro_nome = StringField('Primeiro Nome', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=30)])
-    sobrenome = StringField('Sobrenome', validators=[InputRequired(), Length(min=1, max=100)])
-    email = StringField('Email', validators=[InputRequired(), Email(message=ERRO_EMAIL), Length(min=1, max=254)])
-    senha = PasswordField('Senha', validators=[InputRequired(), EqualTo('confirmacao', message=ERRO_COMPARA_SENHAS), Length(min=8, max=20)])
-    confirmacao = PasswordField('Confirmação de Senha', validators=[InputRequired(), Length(min=8, max=20)])
+    sobrenome = StringField('Sobrenome', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=100)])
+    email = StringField('Email', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Email(message=ERRO_EMAIL), Length(min=1, max=254)])
+    senha = PasswordField('Senha', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), EqualTo('confirmacao', message=ERRO_COMPARA_SENHAS), Length(min=8, max=20, message=ERRO_TAMANHO_SENHA)])
+    confirmacao = PasswordField('Confirmação de Senha', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=8, max=20)])
     curso = SelectField('Curso', choices=get_opcoes_cursos(), id= "curso", coerce=int)
     instituicao = SelectField('Instituição', choices=get_opcoes_instituicoes(), id="instituicao", default="UFSCar", coerce=int)
     cidade = SelectField('Cidade', choices=get_opcoes_cidades(), id="cidade", default="São Carlos", coerce=int)
@@ -27,10 +27,10 @@ class ParticipanteForm(FlaskForm):
 
 class EditarUsuarioForm(FlaskForm):
     primeiro_nome = StringField('Primeiro Nome', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=30)])
-    sobrenome = StringField('Sobrenome', validators=[InputRequired(), Length(min=1, max=100)])
-    email = StringField('Email', validators=[InputRequired(), Email(message=ERRO_EMAIL), Length(min=1, max=254)])
+    sobrenome = StringField('Sobrenome', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=1, max=100)])
+    email = StringField('Email', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Email(message=ERRO_EMAIL), Length(min=1, max=254)])
     curso = SelectField('Curso', choices=get_opcoes_cursos(), id= 'curso', coerce=int)
     instituicao = SelectField('Instituição', choices=get_opcoes_instituicoes(), id='instituicao', coerce=int)
     cidade = SelectField('Cidade', choices=get_opcoes_cidades(), id='cidade', coerce=int)
     data_nasc = DateField("Data de Nascimento", format="%d/%m/%Y", id='data-nasc')
-    senha = PasswordField('Senha', validators=[InputRequired(), Length(min=8, max=20)])
+    senha = PasswordField('Senha', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=8, max=20, message=ERRO_TAMANHO_SENHA)])
