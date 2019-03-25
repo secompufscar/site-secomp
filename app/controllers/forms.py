eb2c795be2956585add8b358fe4c50a15815499a
nathanoliveira19 <nathan.oliveirasc17@gmail.com>
2019-03-25 19:30:02 -0300

    Melhorias nos formulários

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField, BooleanField, SelectField, DateField
 from wtforms.validators import InputRequired, Email, Length, EqualTo
-
 from app.controllers.functions.form_choices import get_opcoes_cidades, get_opcoes_instituicoes, get_opcoes_cursos, get_opcoes_camisetas, get_opcoes_usuarios_permissao, get_opcoes_permissoes
 from app.controllers.functions.custom_form_validators import *
 from app.controllers.constants import *
@@ -16,17 +15,17 @@ class LoginForm(FlaskForm):
 
 class CadastroForm(FlaskForm):
     primeiro_nome = StringField('Primeiro Nome', validators=[InputRequired(
-        message=ERRO_INPUT_REQUIRED), Length(min=1, max=30)])
+        message=ERRO_INPUT_REQUIRED), Length(min=1, max=30), so_letras()], id="primeiro_nome")
     sobrenome = StringField('Sobrenome', validators=[InputRequired(
-        message=ERRO_INPUT_REQUIRED), Length(min=1, max=100)])
+        message=ERRO_INPUT_REQUIRED), Length(min=1, max=100), so_letras()], id="sobrenome")
     email = StringField('Email', validators=[InputRequired(
-        message=ERRO_INPUT_REQUIRED), Email(message=ERRO_EMAIL), Length(min=1, max=254), email_existe()])
+        message=ERRO_INPUT_REQUIRED), Email(message=ERRO_EMAIL), Length(min=1, max=254), email_existe()], id="email")
     senha = PasswordField('Senha', validators=[InputRequired(message=ERRO_INPUT_REQUIRED), EqualTo(
-        'confirmacao', message=ERRO_COMPARA_SENHAS), Length(min=8, max=20, message=ERRO_TAMANHO_SENHA)])
+        'confirmacao', message=ERRO_COMPARA_SENHAS), Length(min=8, max=20, message=ERRO_TAMANHO_SENHA)], id="senha")
     confirmacao = PasswordField('Confirmação de Senha', validators=[
-                                InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=8, max=20)])
+                                InputRequired(message=ERRO_INPUT_REQUIRED), Length(min=8, max=20)], id="confirmacao_senha")
     curso = SelectField('Curso', choices=get_opcoes_cursos(),
-                        id="curso", coerce=int)
+                            id="curso", coerce=int)
     instituicao = SelectField('Instituição', choices=get_opcoes_instituicoes(
     ), id="instituicao", default="UFSCar", coerce=int)
     cidade = SelectField('Cidade', choices=get_opcoes_cidades(
--- app/controllers/functions/custom_form_validators.py
+++ app/controllers/functions/custom_form_validators.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms.validators import ValidationError
 from app.models.models import *
 from flask_login import login_required, login_user, logout_user
+import re
 
 def email_existe():
     mensagem = "Este email já está cadastrado!"
@@ -10,3 +11,11 @@ def email_existe():
         if usuario is not None:
             raise ValidationError(mensagem)
     return _email_existe
+
+def so_letras():
+    mensagem = "Entrada inválida (deve conter apenas letras)"
+    def _so_letras(form, field):
+        s = str(field.data)
+        if re.search("[^a-zA-ZÁÂÃÀÇÉÊÍÓÔÕÚÜáâãàçéêíóôõúü\s]", s):
+            raise ValidationError(mensagem)
+    return _so_letras
