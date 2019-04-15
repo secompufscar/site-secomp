93ab77cbfa2e872686890dcba90b1b8505782442
Gabriel <g4briel.4lves@gmail.com>
2019-04-15 15:14:01 +0000

    Terminando port do Manager

--- app/__init__.py
+++ app/__init__.py
@@ -5,96 +5,103 @@ from flask_babelex import Babel
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from flask_script import Server, Manager
-
-configs = {
-    'development': '../config/development.py',
-    'production': '../config/production.py',
-    'default': '../config/default.py'
-}
-
-config_name = getenv('FLASK_CONFIGURATION', 'development')
-
-app = Flask(__name__)
-app.config.from_pyfile(configs[config_name])
-
-Bootstrap(app)
 
 
-@app.errorhandler(400)
-def bad_request(error):
-    return render_template('400.html'), error
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), error
-
-
-@app.errorhandler(500)
-def internal_server_error(error):
-    return render_template('500.html'), error
-
-
-from app.models.models import db, Usuario
-
-migrate = Migrate(app, db)
-
-from app.controllers.functions.email import mail
-
-mail.init_app(app)
-
-from app.controllers.routes import admin, management, users, views
-
-app.register_blueprint(management.management)
-app.register_blueprint(users.users)
-app.register_blueprint(views.views)
-
-upload_path = path.join(path.dirname(__file__), 'static')
-adm = admin.init_app(app, upload_path)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-
-@login_manager.user_loader
-def user_loader(user_id):
-    return db.session.query(Usuario).filter_by(id=user_id).first()
-
-
-@login_manager.unauthorized_handler
-def unauthorized_callback():
-    return redirect('/login')
-
-
-manager = Manager(app)
-manager.add_command('runserver', Server(host='0.0.0.0'))
-
-
-@app.cli.command()
-def create():
+def create_app(config_name):
     """
-    Creates database tables from sqlalchemy models
+    Project app factory
     """
-    db.create_all()
+    
+    configs = {
+        'development': '../config/development.py',
+        'production': '../config/production.py',
+        'default': '../config/default.py'
+    }
+    
+    if config_name not in configs:
+        config_name = 'default'
+
+    app = Flask(__name__)
+    app.config.from_pyfile(configs[config_name])
+
+    Bootstrap(app)
 
 
-@app.cli.command()
-def drop():
-    """
-    Drops database tables
-    """
-    prompt = input('Erase current database? [y/n]')
-    if prompt == 'y':
-        db.session.close_all()
-        db.drop_all()
+    @app.errorhandler(400)
+    def bad_request(error):
+        return render_template('400.html'), error
 
 
-babel = Babel(app)
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('404.html'), error
 
 
-@babel.localeselector
-def get_locale():
-    if request.args.get('lang'):
-        session['lang'] = request.args.get('lang')
-    return "pt"
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('500.html'), error
+
+
+    from app.models.models import db, Usuario
+    
+    app.app_context().push()
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+
+    @app.cli.command()
+    def create():
+        """
+        Creates database tables from sqlalchemy models
+        """
+        db.create_all()
+
+
+    @app.cli.command()
+    def drop():
+        """
+        Drops database tables
+        """
+        prompt = input('Erase current database? [y/n]')
+        if prompt == 'y':
+            db.session.close_all()
+            db.drop_all()
+
+
+    from app.controllers.functions.email import mail
+
+    mail.init_app(app)
+
+    from app.controllers.routes import admin, management, users, views
+
+    app.register_blueprint(management.management)
+    app.register_blueprint(users.users)
+    app.register_blueprint(views.views)
+
+    upload_path = path.join(path.dirname(__file__), 'static')
+    adm = admin.init_app(app, upload_path)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+
+    @login_manager.user_loader
+    def user_loader(user_id):
+        return db.session.query(Usuario).filter_by(id=user_id).first()
+
+
+    @login_manager.unauthorized_handler
+    def unauthorized_callback():
+        return redirect('/login')
+
+
+    babel = Babel(app)
+
+
+    @babel.localeselector
+    def get_locale():
+        if request.args.get('lang'):
+            session['lang'] = request.args.get('lang')
+        return "pt"
+
+    return app
---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import os
+from app import create_app
+
+if __name__ == '__main__':
+    config = os.getenv('FLASK_CONFIGURATION', 'development')
+    app = create_app(config)
+    app.run('0.0.0.0')
+
