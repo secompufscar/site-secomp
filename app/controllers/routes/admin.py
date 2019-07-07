6b1c93fe70e3d7d3342eb95c5248b97a8fb5d759
nathanoliveira19 <nathan.oliveirasc17@gmail.com>
2019-07-07 01:16:16 -0300

    Corrigindo condições de acesso ao /admin

--- app/controllers/routes/admin.py
+++ app/controllers/routes/admin.py
@@ -11,7 +11,7 @@ from app.models.models import *
 class AppIndexView(AdminIndexView):
     @expose('/')
     def index(self):
-        if current_user.is_authenticated and current_user.is_admin:
+        if current_user.autenticado and current_user.admin:
             self._template_args['usuario'] = current_user
             return super(AppIndexView, self).index()
         return redirect(url_for('views.index'))
@@ -24,7 +24,7 @@ class AppModelView(ModelView):
 
     @classmethod
     def is_accessible(cls):
-        return current_user.is_authenticated and current_user.is_admin
+        return current_user.autenticado and current_user.admin
 
     @classmethod
     def inaccessible_callback(cls, name, **kwargs):
