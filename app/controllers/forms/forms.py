0db2f373f58b91a929776cedcbca193b3f168f90
nathanoliveira19 <nathan.oliveirasc17@gmail.com>
2019-08-05 19:49:45 -0300

    Inserindo maxlength na renderização html do wtforms

--- app/controllers/forms/forms.py
+++ app/controllers/forms/forms.py
@@ -129,9 +129,9 @@ class ListasParticipantes(FlaskForm):
 
 class CadastroMinistranteForm(FlaskForm):
     primeiro_nome = StringField('Primeiro Nome', validators=[InputRequired(
-        message=ERRO_INPUT_REQUIRED), Length(min=1, max=30), so_letras()], id="primeiro_nome")
+        message=ERRO_INPUT_REQUIRED), Length(min=1, max=64), so_letras()], id="primeiro_nome", render_kw={'maxlength': 64})
     sobrenome = StringField('Sobrenome', validators=[InputRequired(
-        message=ERRO_INPUT_REQUIRED), Length(min=1, max=100), so_letras()], id="sobrenome")
+        message=ERRO_INPUT_REQUIRED), Length(min=1, max=64), so_letras()], id="sobrenome", render_kw={'maxlength': 64})
     email = StringField('Email', validators=[InputRequired(
         message=ERRO_INPUT_REQUIRED), Email(message=ERRO_EMAIL), Length(min=1, max=254),
         valida_email_ministrante()], id="email")
@@ -142,11 +142,11 @@ class CadastroMinistranteForm(FlaskForm):
     data_nascimento = DateField("Data de Nascimento",
                           format="%d/%m/%Y", id="data_nascimento")
     telefone = StringField('Telefone', validators=[InputRequired(), Length(min=8, max=14)], id='telefone')
-    profissao = StringField('Profissão', validators=[InputRequired(), Length(min=1, max=64)], id='profissao')
+    profissao = StringField('Profissão', validators=[InputRequired(), Length(min=1, max=64)], id='profissao', render_kw={'maxlength': 64})
     empresa_universidade = StringField('Empresa/Universidade', id='empresa_universidade',
         validators=[Length(max=64)])
     biografia = TextAreaField('Breve descrição biográfica, a ser utilizada na divulgação', validators=[InputRequired(),
-        Length(min=1, max=1500)], id='biografia')
+        Length(min=1, max=1500)], id='biografia', render_kw={'maxlength': 1500})
     foto = FileField('Foto', validators=[
             FileAllowed(['png', 'jpg', 'jpeg'], message=ERRO_EXTENSAO_INVALIDA)
         ])
@@ -160,12 +160,13 @@ class CadastroMinistranteForm(FlaskForm):
     codigo_url = ''
 
 class CadastroInformacoesMinicurso(FlaskForm):
-    titulo = StringField('Título do Minicurso', validators=[InputRequired(), Length(min=1,max=256)], id='titulo')
+    titulo = StringField('Título do Minicurso', validators=[InputRequired(), Length(min=1,max=256)], id='titulo', render_kw={'maxlength': 256})
     descricao = TextAreaField('Descrição', validators=[InputRequired(),
-        Length(min=1,max=1024)], id='descricao')
+        Length(min=1,max=1024)], id='descricao', render_kw={'maxlength': 1024})
     pre_requisitos = TextAreaField('Pré-requisitos recomendados aos participantes', validators=[InputRequired(),
-        Length(max=512)], id='pre_requisitos')
-    planejamento = TextAreaField('Descrição da estrutura do minicurso', validators=[InputRequired(), Length(max=2056)], id='planejamento')
+        Length(max=512)], id='pre_requisitos', render_kw={'maxlength': 512})
+    planejamento = TextAreaField('Descrição da estrutura do minicurso', validators=[InputRequired(), Length(max=2056)], id='planejamento',
+                                 render_kw={'maxlength': 2056})
     apresentacao_extra = FileField('Apresentação extra', validators=[
         FileAllowed(['pdf', 'doc', 'docx', 'ppt', 'pptx','rar', 'zip', 'tar', 'z', 'gz', 'taz', 'tgz'],
                     message=ERRO_EXTENSAO_INVALIDA)
@@ -175,52 +176,52 @@ class CadastroInformacoesMinicurso(FlaskForm):
                     message=ERRO_EXTENSAO_INVALIDA)
         ], id='material')
     requisitos_ide = TextAreaField('Existe alguma preferência de IDE (Ambiente Integral de Desenvolvimento) ou editor de texto para o minicurso?',
-                                    validators=[InputRequired(), Length(max=500)], id='requisitos_ide')
+                                    validators=[InputRequired(), Length(max=500)], id='requisitos_ide', render_kw={'maxlength': 500})
     requisitos_bibliotecas_pacotes = TextAreaField('Caso existam, especifique todos os pacotes e/ou bibliotecas adicionais que serão utilizados no decorrer do minicurso',
-                                                   validators=[InputRequired(), Length(max=500)], id='requisitos_bibliotecas_pacotes')
+                                                   validators=[InputRequired(), Length(max=500)], id='requisitos_bibliotecas_pacotes', render_kw={'maxlength': 500})
     requisitos_dependencias = TextAreaField('Especifique todos os programas e dependências que serão necessários no decorrer do minicurso',
-                                                   validators=[InputRequired(), Length(max=500)], id='requisitos_dependencias')
+                                                   validators=[InputRequired(), Length(max=500)], id='requisitos_dependencias', render_kw={'maxlength': 500})
     requisitos_sistema = TextAreaField('Caso exista, especifique alguma limitação no uso de algum sistema operacional para o minicurso',
-                                                   validators=[Length(max=500)], id='requisitos_sistema')
+                                                   validators=[Length(max=500)], id='requisitos_sistema', render_kw={'maxlength': 500})
     requisitos_observacoes = TextAreaField('Existe alguma observação em relação aos requisitos do minicurso?',
-                                                   validators=[Length(max=500)], id='requisitos_observacoes')
+                                                   validators=[Length(max=500)], id='requisitos_observacoes', render_kw={'maxlength': 500})
     requisitos_github = StringField('No caso da existência de código disponível no GitHub ou em outros repositórios, forneça o link para este repositório ',
-                                                   validators=[Length(max=500)], id='requisitos_github')
+                                                   validators=[Length(max=500)], id='requisitos_github', render_kw={'maxlength': 500})
     requisitos_hardware = TextAreaField('Caso o minicurso envolva hardware, forneça os requisitos de hardware',
-                                        validators=[Length(max=500)], id='requisitos_hardware')
-    dicas_instalacao = TextAreaField('Dicas para instalação dos softwares necessários', id='dicas_instalacao', validators=[Length(max=500)])
-    observacoes = TextAreaField('Observações em geral', id='observacoes', validators=[Length(max=600)])
+                                        validators=[Length(max=500)], id='requisitos_hardware', render_kw={'maxlength': 500})
+    dicas_instalacao = TextAreaField('Dicas para instalação dos softwares necessários', id='dicas_instalacao', validators=[Length(max=500)], render_kw={'maxlength': 500})
+    observacoes = TextAreaField('Observações em geral', id='observacoes', validators=[Length(max=600)], render_kw={'maxlength': 600})
     confirmacao = BooleanField('Li e concordo com Termo e Condições de Inscrição', validators=[InputRequired()])
 
 class CadastroInformacoesPalestra(FlaskForm):
-    titulo = StringField('Título da Palestra', validators=[InputRequired(), Length(min=1,max=256)], id='titulo')
-    descricao = TextAreaField('Descrição', validators=[InputRequired(), Length(min=1,max=1024)], id='descricao')
-    requisitos_tecnicos = TextAreaField('Requisitos de Hardware/Software', id='requisitos_tecnicos')
-    planejamento = TextAreaField('Planejamento', validators=[InputRequired(), Length(max=2056)], id='planejamento')
+    titulo = StringField('Título da Palestra', validators=[InputRequired(), Length(min=1,max=256)], id='titulo', render_kw={'maxlength': 256})
+    descricao = TextAreaField('Descrição', validators=[InputRequired(), Length(min=1,max=1024)], id='descricao', render_kw={'maxlength': 1024})
+    requisitos_tecnicos = TextAreaField('Requisitos de Hardware/Software', id='requisitos_tecnicos', validators=[Length(max=1024)], render_kw={'maxlength': 1024})
+    planejamento = TextAreaField('Planejamento', validators=[InputRequired(), Length(max=2056)], id='planejamento', render_kw={'maxlength': 2056})
     material = FileField('Material', validators=[
         FileAllowed(['pdf', 'doc', 'docx', 'ppt', 'pptx', 'rar', 'zip', 'tar', 'z', 'gz', 'taz', 'tgz'],
                     message=ERRO_EXTENSAO_INVALIDA)
         ], id='material')
-    perguntas = TextAreaField('Perguntas referentes à palestra', validators=[InputRequired()], id='perguntas')
-    observacoes = TextAreaField('Observações', id='observacoes', validators=[Length(max=600)])
+    perguntas = TextAreaField('Perguntas referentes à palestra', validators=[InputRequired(), Length(max=1024)], id='perguntas', render_kw={'maxlength': 1024})
+    observacoes = TextAreaField('Observações', id='observacoes', validators=[Length(max=600)], render_kw={'maxlength': 600})
     confirmacao = BooleanField('Li e concordo com Termo e Condições de Inscrição', validators=[InputRequired()], id='confirmacao')
 
 class CadastroFeiraDeProjetos(FlaskForm):
-    titulo = StringField('Título do Projeto', validators=[InputRequired(), Length(min=1,max=256)], id='titulo')
-    descricao = TextAreaField('Descrição', validators=[InputRequired(), Length(min=1, max=1024)], id='descricao')
-    necessidades = TextAreaField('Necessidades', validators=[InputRequired()], id='necessidades')
-    planejamento = TextAreaField('Planejamento', validators=[InputRequired(), Length(max=2056)], id='planejamento')
-    observacoes = TextAreaField('Observações', id='observacoes', validators=[Length(max=600)])
+    titulo = StringField('Título do Projeto', validators=[InputRequired(), Length(min=1,max=256)], id='titulo', render_kw={'maxlength': 256})
+    descricao = TextAreaField('Descrição', validators=[InputRequired(), Length(min=1, max=1024)], id='descricao', render_kw={'maxlength': 1024})
+    necessidades = TextAreaField('Necessidades', validators=[InputRequired(), Length(max=1024)], id='necessidades', render_kw={'maxlength': 1024})
+    planejamento = TextAreaField('Planejamento', validators=[InputRequired(), Length(max=2056)], id='planejamento', render_kw={'maxlength': 2056})
+    observacoes = TextAreaField('Observações', id='observacoes', validators=[Length(max=600)], render_kw={'maxlength': 600})
     confirmacao = BooleanField('Li e concordo com Termo e Condições de Inscrição', validators=[InputRequired()])
 
 class CadastroAtividadeGenerica(FlaskForm):
-    titulo = StringField('Título da Atividade', validators=[InputRequired(), Length(min=1,max=256)], id='titulo')
-    descricao = TextAreaField('Descrição', validators=[InputRequired(), Length(min=1, max=1024)], id='descricao')
-    observacoes = TextAreaField('Observações', id='observacoes', validators=[Length(max=600)])
+    titulo = StringField('Título da Atividade', validators=[InputRequired(), Length(min=1,max=256)], id='titulo', render_kw={'maxlength': 256})
+    descricao = TextAreaField('Descrição', validators=[InputRequired(), Length(min=1, max=1024)], id='descricao', render_kw={'maxlength': 1024})
+    observacoes = TextAreaField('Observações', id='observacoes', validators=[Length(max=600)], render_kw={'maxlength': 600})
     confirmacao = BooleanField('Li e concordo com Termo e Condições de Inscrição', validators=[InputRequired()])
 
 class CadastroInformacoesLocomocaoEstadia(FlaskForm):
-    cidade_origem = StringField('Cidade de origem', validators=[InputRequired(), Length(min=1,max=64)])
+    cidade_origem = StringField('Cidade de origem', validators=[InputRequired(), Length(min=1,max=64)], render_kw={'maxlength': 64})
     data_chegada_sanca = DateField('Data de chegada em São Carlos', format='%d/%m/%Y', id='data_chegada_sanca',
         validators=[InputRequired()])
     data_partida_sanca = DateField('Data de partida de São Carlos', format='%d/%m/%Y', id='data_partida_sanca',
@@ -238,7 +239,7 @@ class CadastroInformacoesLocomocaoEstadia(FlaskForm):
     necessidades_hospedagem = TextAreaField('Quais são as necessidades básicas a serem atendidas pela estadia?',
         id='necessidades_hospedagem', validators=[Length(max=256), RequiredIf(hospedagem=True)])
     observacoes = TextAreaField('Deixe aqui alguma observação ou informação que julgar necessária', id='hospedagem',
-        validators=[Length(max=256)])
+        validators=[Length(max=256)], render_kw={'maxlength': 256})
 
 class GerarUrlConteudoForm(FlaskForm):
     tipo_atividade = SelectField("Tipo da Atividade", choices=get_opcoes_tipo_atividade(), id="tipo_atividade", coerce=int, validators=[InputRequired()])
